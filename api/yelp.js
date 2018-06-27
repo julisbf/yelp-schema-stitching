f147e8c7d5daa08a8f596f7cefbacf7344fa987c
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-06-27 11:05:43 -0400

    Create `yelp.js` with api connection

---
+++ api/yelp.js
@@ -0,0 +1,54 @@
+const rp = require('request-promise')
+require('dotenv').config();
+
+const YELP_API_KEY = process.env.ACCESS_TOKEN
+
+function query(location, term, limit) {
+  let QUERY = `
+    {
+      search(term: '${term}',
+        location: '${location}',
+        limit: '${limit}') {
+          business{
+            ...bizInfo
+          }
+        }
+    }
+
+    fragment bizInfo on Business {
+      id
+      name
+      price
+      photos
+      coordinates {
+        latitude
+        longitude
+      }
+      location {
+        address1
+        city
+        state
+        country
+      }
+      categories {
+        title
+      }
+    }
+  `
+
+  const options = {
+    method: 'POST',
+    uri: 'https://api.yelp.com/v3/graphql',
+    body: QUERY,
+    json: false,
+    headers: {
+      'Content-Type': 'application/graphql',
+      'Authorization': `Bearer ${YELP_API_KEY}`
+    }
+  };
+  return rp(options).then(html => JSON.parse(html));
+}
+
+module.exports = {
+  query
+}
