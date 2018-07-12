de455e215253d5abe702cec3884623e438fb44cc
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-07-12 10:55:06 -0400

    Remove unused code

---
+++ src/YelpBinding.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const fetch = require('node-fetch')
+const {
+  Binding
+} = require('graphql-binding')
+const {
+  HttpLink
+} = require('apollo-link-http')
+const {
+  makeRemoteExecutableSchema
+} = require('graphql-tools')
+const {
+  setContext
+} = require('apollo-link-context')
+const yelpTypes = require('./generated/yelp.js')
+
+const DELIVERY_API_BASE_URL = 'https://api.yelp.com/v3/graphql'
+const YELP_API_KEY = process.env.ACCESS_TOKEN
+
+class YelpBinding extends Binding {
+  constructor() {
+    const endpoint = DELIVERY_API_BASE_URL
+    const link = new HttpLink({
+      fetch,
+      uri: endpoint,
+      headers: {
+        'Authorization': `Bearer ${YELP_API_KEY}`
+      }
+    })
+
+    const schema = makeRemoteExecutableSchema({
+      link,
+      schema: yelpTypes
+    })
+    super({
+      schema
+    })
+  }
+}
+
+module.exports = YelpBinding
